ba3bf3d7283b4fcea760cd578413e141541f9335
IanHernandez95 <ian.hernandez.f@gmail.com>
2024-03-07 11:08:48 -0300

    fix: borrar cambio de idioma y modificar titulo

--- assets/js/main.js
+++ assets/js/main.js
@@ -247,18 +247,3 @@
 })()
 
 
-/**
-   * Cambio de idioma 
-   */
-
-var check=document.querySelector(".check");
-check.addEventListener('click',idioma);
-
-function idioma(){
-  let id=check.checked;
-  if (id==true){
-    location.href="en/index.html";
-  }else{
-    location.href="../index.html";
-  }
-}
